19456a7a4644b9153425bef033149d39b0f20d96
sergey-dev <serbro7@gmail.com>
2022-04-30 13:42:13 +0200

    Product image column migration
    Migration created for adding the image column to Productos table. Datatype: mediumblob (16MB)

---
+++ backend-master/database/migrations/2022_04_30_112252_product_image_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class ProductImageColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            //$table->longblog('imagen')->nullable();
+        });
+        DB::statement("ALTER TABLE productos ADD imagen MEDIUMBLOB");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->dropColumn('imagen');
+        });
+    }
+}
